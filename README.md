dc8885aa48ddcaded7ff4e0736d55ddfc2097b6e
Noah Prowell <noahprowell@gmail.com>
2024-08-05 15:44:01 -0600

    updated final urls

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 - multi graph webpage shwoing current snow recordings and snow depth in certain areas of colorado.
 - each graph will be a station where the snow levels have been recorded.
 - Possibly have a few stations showing the last 15 minute precipitation numbers for a real time pow level meeter.
-colorado request URL https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=SNOW&locationid=FIPS:08&startdate=2023-10-31&enddate=2024-06-10&limit=1000
 
 api docs: https://www.ncdc.noaa.gov/cdo-web/webservices/v2#locations
 
-stations: VAIL 0.9 WNW, CO US, AVON 1.1 SE, CO US, GRANBY 2.9 NE, CO US, LEADVILLE 1.4 WNW, CO US, CRESTED BUTTE 1.9 NNE, CO US, PAGOSA SPRINGS 6.1 NE, CO US, BASALT 5.8 E, CO US, EVERGREEN 2.6 S, CO US
+snow depth: https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=SNWD&locationid=FIPS:08&stationid=GHCND:USS0005K14S&units=standard&startdate=2024-01-07&enddate=2024-01-14&limit=1000
+
+precipitation: https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=PRCP&locationid=FIPS:08&stationid=GHCND:USS0005K14S&units=standard&startdate=2024-01-07&enddate=2024-01-14&limit=1000
--- initial_eda.py
+++ initial_eda.py
@@ -1,14 +1,27 @@
 import requests
 import pandas as pd
 #url 
-base_url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=PRCP&locationid=FIPS:08&startdate=2023-10-31&enddate=2024-06-10&limit=1000'
-locations_url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/stations?datasetid=GHCND&datatypeid=PRCP&locationid=FIPS:08&startdate=2023-10-31&enddate=2024-06-10&limit=1000&offeset=999'
-data_categories = 'https://www.ncei.noaa.gov/cdo-web/api/v2/datacategories?limit=45'
-data_types = 'https://www.ncei.noaa.gov/cdo-web/api/v2/datatypes'
+base_url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=SNWD&locationid=FIPS:08&stationid=GHCND:USS0005K14S&units=standard&startdate=2024-01-07&enddate=2024-01-14&limit=1000'
+locations_url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/stations?datasetid=GHCND&datatypeid=SNWD&locationid=FIPS:08&startdate=2024-01-07&enddate=2024-01-14&limit=1000&offeset=0'
+data_categories = 'https://www.ncei.noaa.gov/cdo-web/api/v2/datacategories?enddate=2024-06-10&offset=45&limit=1000'
+data_types = 'https://www.ncei.noaa.gov/cdo-web/api/v2/datatypes?datasetid=GHCND&locationid=FIPS:08&enddate=2024-06-10&offset=0&limit=1000'
+datasets = 'https://www.ncei.noaa.gov/cdo-web/api/v2/datasets?enddate=2024-06-10&dataypeid=PRCP&locationid=FIPS:08'
+#datatypes: Precipitation (PRCP), Snow depth (SNWD)  
 
 
+
+
+
+
+
+
+
+
+snow_depth_url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=SNWD&locationid=FIPS:08&stationid=GHCND:USS0005K14S&units=standard&startdate=2024-01-07&enddate=2024-01-14&limit=1000'
+precipitation_url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=PRCP&locationid=FIPS:08&stationid=GHCND:USS0005K14S&units=standard&startdate=2024-01-07&enddate=2024-01-14&limit=1000'
+#precipitation and snow depth in inches
 headers = {'token':'ptCSsKJigBiLLmlIdcjtNPMGhdYTpiXG'}
-r = requests.get(url = locations_url, headers=headers)
+r = requests.get(url = precipitation_url, headers=headers)
 locdf = pd.json_normalize(r.json()['results'])
 
 # r = requests.get(url = base_url, headers=headers)
@@ -17,14 +30,14 @@ locdf = pd.json_normalize(r.json()['results'])
 # loc = requests.get(url = locations_url, headers=headers)
 # locations = pd.json_normalize(loc.json()['results'])
 # merged = pd.merge(datadf, locdf,how='left',left_on='station',right_on='id')
-import folium
-m = folium.Map(location=[locdf['latitude'].mean(), locdf['longitude'].mean()], zoom_start=5)
+# import folium
+# m = folium.Map(location=[locdf['latitude'].mean(), locdf['longitude'].mean()], zoom_start=5)
 
-for i, row in locdf.iterrows():
-    folium.Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=row['name']
-    ).add_to(m)
+# for i, row in locdf.iterrows():
+#     folium.Marker(
+#         location=[row['latitude'], row['longitude']],
+#         popup=row['name']
+#     ).add_to(m)
 
-# Display the map
-m.save('map.html')
+# # Display the map
+# m.save('names.html')
